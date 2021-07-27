0fff2be796c2a385455fcf10e80579f85b1a2608
pratikdevle <87975916+pratikdevle@users.noreply.github.com>
2021-07-27 11:30:52 +0530

    Update Controller1.java

--- src/main/java/test1/controller/Controller1.java
+++ src/main/java/test1/controller/Controller1.java
@@ -1,5 +1,6 @@
 package test1.controller;
-
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
@@ -15,11 +16,21 @@ public class Controller1 {
 	public ResponseEntity<?> hello(@RequestBody String payload) {
 		System.out.println("Entered Inquiry Service at Node 1");
 		System.out.println(payload);
+		InetAddress ip = null;
+		String hostName = "";
+		try {
+			ip = InetAddress.getLocalHost();
+			hostName = ip.getHostName();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
 		MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
 		headers.add("Content-Type", "application/xml");
 		String response = "<Student>\r\n"
 							+"<Name>Pratik</Name>\r\n"
 							+"<City>Pune</City>\r\n"
+										+"<Address>"+ip +"</Address>\r\n"
+							+"<Hostname>"+hostName+"</Hostname>\r\n"
 						+"</Student>";
 		return new ResponseEntity<>(response, headers, HttpStatus.OK);
 	}
